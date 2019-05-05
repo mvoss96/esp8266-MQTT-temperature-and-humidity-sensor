7a1905ed90ed2920bfd80d97935901c8167b8d9b
Marcus Voss <m.voss@laposte.net>
2019-05-05 21:19:48 +0200

    added comments

--- TempHumidMQTT.ino
+++ TempHumidMQTT.ino
@@ -2,35 +2,37 @@
 #include <PubSubClient.h>
 #include <DHT.h>
 
-#define DEVICE_NAME "mySens-01"
-#define MQTT_SERVER_IP "192.168.0.111"
-#define QCONNECT
-#define CONNECT_TIMEOUT 150
-#define INTERVAL 5e6
+#define DEVICE_NAME "mySens-01" //This name is used for both WIFI and MQTT. Must be unique.
+#define MQTT_SERVER_IP "192.168.0.111" //Address of the MQTT Broker.
+#define QCONNECT //Accelerates wifi-connection after deep sleep. 
+#define CONNECT_TIMEOUT 150 //Number of attempts before wifi connection times out.
+#define INTERVAL 5e6 //Duration of sleep cycles 
 #define WIFI_SSID "Eine Scheibe Brot"
 #define WIFI_PASSWORD "T3m$7gHQ3Z"
-IPAddress IP(192, 168, 0, 33);
+IPAddress IP(192, 168, 0, 33); //Static ip
 IPAddress GATEWAY(192, 168, 0, 1);
 IPAddress SUBNET(255, 255, 255, 0);
 IPAddress DNS(192, 168, 0, 1);
 
-
-char msg[50];
-float tmp = 0;
-float humidity = 0;
-long bootTime;
+long bootTime; //Used to calculate code execution time
 WiFiClient espClient;
 PubSubClient client(espClient);
 DHT dht(D2, DHT22);
 
-
+/* Struct rtcData: used to store wifi parameters inside the rtc memory during deep sleep.
+    This is necessary for wifi quick connect. Must be a multiple of 4 bytes*/
 struct {
-  uint32_t crc32;   // 4 bytes
-  uint8_t wifi_channel;  // 1 byte,   5 in total
-  uint8_t bSSID[6]; // 6 bytes, 11 in total
-  uint8_t padding;  // 1 byte,  12 in total
+  uint32_t crc;
+  uint8_t wifi_channel;
+  uint8_t bSSID[6];
+  uint8_t reserved ;
 } rtcData;
 
+/*Taken from the RTCUserMemory example. This function calculates a crc32 ckecksum for given data.
+   Used to ensure dada validity. The Data must be alligned for this function to work.
+   Arguments:
+ * *data -> pointer to starting address of the data
+   length -> lenght of the data */
 uint32_t calculateCRC32( const uint8_t *data, size_t length ) {
   uint32_t crc = 0xffffffff;
   while ( length-- ) {
@@ -49,39 +51,42 @@ uint32_t calculateCRC32( const uint8_t *data, size_t length ) {
   }
   return crc;
 }
+
+//This function stores the current wifi channel and bSSID in the rtc memory.
 void storeRTC() {
   rtcData.wifi_channel = WiFi.channel();
   memcpy( rtcData.bSSID, WiFi.BSSID(), 6 );
-  rtcData.crc32 = calculateCRC32( ((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4 );
+  rtcData.crc = calculateCRC32( ((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4 );
   ESP.rtcUserMemoryWrite( 0, (uint32_t*)&rtcData, sizeof( rtcData ) );
 }
 
+// Connect to the wifi network
 void setup_wifi() {
+  bootTime = millis();
   int tries = 0;
-  bool rtcValid = false;
+  bool rtcOK = false;
   WiFi.mode(WIFI_STA);
   WiFi.hostname(DEVICE_NAME);
   WiFi.config(IP, GATEWAY, SUBNET);
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WIFI_SSID);
-
 #ifdef QCONNECT
   Serial.println("(quick connect)");
-  if ( ESP.rtcUserMemoryRead( 0, (uint32_t*)&rtcData, sizeof( rtcData ) ) ) {
-    // Calculate the CRC of what we just read from RTC memory, but skip the first 4 bytes as that's the checksum itself.
+  if ( ESP.rtcUserMemoryRead( 0, (uint32_t*)&rtcData, sizeof( rtcData ) ) )
+  {
+    //first 4 bytes are skipped because they contain the checksum itself
     uint32_t crc = calculateCRC32( ((uint8_t*)&rtcData) + 4, sizeof( rtcData ) - 4 );
-    if ( crc == rtcData.crc32 ) {
-      rtcValid = true;
+    if ( crc == rtcData.crc ) {
+      rtcOK = true;
     }
   }
-  if ( rtcValid ) {
-    // The RTC data was good, make a quick connection
+  if ( rtcOK ) {
     WiFi.begin( WIFI_SSID, WIFI_PASSWORD, rtcData.wifi_channel, rtcData.bSSID, true);
   }
   else {
-    // The RTC data was not valid, so make a regular connection
-    Serial.println("crc invalid! Attempting regular connect");
+    // If the RTC data was not valid, we attempt a regular slower connection
+    Serial.println("crc checksum not correct! Attempting regular connect");
     WiFi.begin( WIFI_SSID, WIFI_PASSWORD );
   }
 #else
@@ -98,6 +103,7 @@ void setup_wifi() {
     Serial.println("failed to connect!");
 #ifdef QCONNECT
     Serial.println("attempting regular connect");
+    //Discard previous wifi parameters and try a normal connect.
     WiFi.disconnect();
     delay( 100 );
     WiFi.forceSleepBegin();
@@ -116,6 +122,7 @@ void setup_wifi() {
     {
       Serial.println("going to sleep to try again later");
       WiFi.disconnect();
+      //If no wifi connection could be made go to sleep for 3s and try again.
       ESP.deepSleep(3e6);
     }
   }
@@ -134,6 +141,7 @@ void setup_wifi() {
 bool send_values_MQTT()
 {
   int attempts = 0;
+  char msg[50];  //buffer for MQTT messages
   dht.begin();
   client.setServer(MQTT_SERVER_IP, 1883);
   while (!client.connected()) {
@@ -150,17 +158,16 @@ bool send_values_MQTT()
       delay(20);
     }
   }
-  tmp = dht.readTemperature();
-  humidity = dht.readHumidity();
-  Serial.println("temp: " + String(tmp));
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  Serial.println("temp: " + String(temperature));
   Serial.println("hum: " + String(humidity));
-  dtostrf(tmp, 6, 2, msg);
+  dtostrf(temperature, 6, 2, msg);
   dtostrf(humidity, 6, 2, msg);
-  return client.publish("homie/"DEVICE_NAME"/DHT22/temperature", msg, true) & client.publish("homie/"DEVICE_NAME"/DHT22/humidity", msg, true);
+  return client.publish(DEVICE_NAME"/DHT22/temperature", msg, true) & client.publish(DEVICE_NAME"/DHT22/humidity", msg, true);
 }
 
 void setup() {
-  bootTime = millis();
   Serial.begin(115000);
   setup_wifi();
   if (!send_values_MQTT())
